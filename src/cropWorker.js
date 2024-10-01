feced173663021305fae38dc29227e4da9a16226
Anna Liu <aliu426@uwo.ca>
2024-10-01 00:01:20 -0400

    cropping done using web worker

---
+++ src/cropWorker.js
@@ -0,0 +1,35 @@
+//message listener for worker thread
+self.onmessage = function (event) {
+    const { imageData } = event.data;
+
+}
+
+//crop function
+const cropImage = (jointData, image) => {//crops each joint from original xray image
+    return new Promise ((resolve,reject) => {
+        const jointImage = new Image();
+        jointImage.src = image;
+
+        jointImage.onload = () => {
+            const canvas = document.createElement('canvas');
+            const ctx = canvas.getContext('2d');
+
+            canvas.width = jointData.width;
+            canvas.height = jointData.height;
+
+            ctx.drawImage(
+                jointImage,
+                jointData.x, jointData.y,
+                0,0,
+                jointData.width, jointData.height
+            );
+
+            const croppedImage = canvas.toDataURL('joint/png');
+            resolve(croppedImage);
+        };
+
+        jointImage.onerror = () => {
+            reject(new Error("Failed to load image"));
+        };
+    });
+}
